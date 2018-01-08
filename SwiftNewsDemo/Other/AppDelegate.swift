6381f3c272914ac6db0a6d5c93638898a7dc8a53
jimingxin <jmx890610@gmail.com>
2018-01-08 17:08:29 +0800

    删除了

--- SwiftNewsDemo/Other/AppDelegate.swift
+++ SwiftNewsDemo/Other/AppDelegate.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = advertiseVC
         window?.makeKeyAndVisible()
         
-        print("sss")
         return true
     }
 
